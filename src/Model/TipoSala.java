6a950e8eae46998b6fa2d3b58ebcd41ee44e4624
soaressf <soaressf@soaressfptmwd>
2013-05-10 20:55:41 +0100

    Criado enumerado TipoSala

---
+++ src/Model/TipoSala.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author soaressf
+ */
+public enum TipoSala {
+    LABORATÓRIO, ANFITEATRO
+}
