7338cfabf23609713481a536d6538bceb3580cb6
soaressf <soaressf@soaressfptmwd>
2013-05-10 23:56:12 +0100

    Melhoramento de todas as classes e métodos da classe Sala.

--- src/Model/Sala.java
+++ src/Model/Sala.java
@@ -43,7 +43,7 @@ public class Sala {
      * @param codigo the codigo to set
      */
     public void setCodigo(String codigo) {
-        this.codigo = codigo;
+        this.codigo = codigo.isEmpty() ? "sem código" : codigo;
     }
 
     /**
@@ -57,7 +57,7 @@ public class Sala {
      * @param tipo the tipo to set
      */
     public void setTipo(TipoSala tipo) {
-        this.tipo = tipo;        
+        this.tipo = tipo; 
     }
 
     /**
@@ -71,12 +71,30 @@ public class Sala {
      * @param capacidade the capacidade to set
      */
     public void setCapacidade(int capacidade) {
-        this.capacidade = capacidade;
+        this.capacidade = (capacidade >= 0) ? capacidade : 0;
     }
     
     private void setSala(String codigo, TipoSala tipo, int capacidade) {
         setCodigo(codigo);
         setTipo(tipo);
         setCapacidade(capacidade);
+    }
+    
+    @Override
+    public String toString() {
+        return null;
+    }
+    
+    @Override
+    public Sala clone() {
+        return new Sala(this);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;        
+        Sala s = (Sala) obj;
+        return codigo.equalsIgnoreCase(s.codigo);
     }
 }
